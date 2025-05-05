70e850d1ba3b71c532aaf53050d4e70e2ad1f403
kdrglvdy <KdrGlvdy@gmail.com>
2025-05-05 01:05:42 -0700

    Update Algorithm.h

--- Micromouse/src/Algorithm.h
+++ Micromouse/src/Algorithm.h
@@ -1,12 +1,12 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 
-void setup();
 void loop();
 void goForward();
 void orientLeft();
 void orientRight();
 void turnAround();
 void stop();
+int wallDetection(int reading);
 
 #endif
