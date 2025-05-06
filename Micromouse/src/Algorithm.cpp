0d6f0ce6d5f735652695ab8dda9fda7b7d771f49
kdrglvdy <KdrGlvdy@gmail.com>
2025-05-06 07:49:35 -0700

    Update Algorithm.cpp
    Integrated StepperMotor.cpp functions with Algorithm.cpp. Deleted turnAround() function since we're not using it.

--- Micromouse/src/Algorithm.cpp
+++ Micromouse/src/Algorithm.cpp
@@ -1,49 +1,32 @@
 #include "Algorithm.h"
 #include "timer.h"
 
+void turnOnMotors() {
+    PORTC |= ((1 << PC4) | (1 << PC5)); // Turn on motors
+}
 
+void goForward() {     
+     rightMotorForward();
+     leftMotorForward(); 
+     
+     Serial.println("Moving forward");
 
-
-void goForward() {
-
-    //PORTC |= (1 << PC4) | (1 << PC5);
-    // changeDutyCycle1(1023);
-    // changeDutyCycle2(1023);
-    Serial.println("Moving forward");
-
-   // PORTC &= ~((1 << PC4) | (1 << PC5));
-    // Set motor to maximimum speed
-    // Both wheels spin forward at max speed
-   //delayS(1);
-    // Set motor to zero speed
-    // Wheels stop spinning
+     delayMs(1000);
 }
 
 void orientLeft() {
-    //PORTC |= (1 << PC4) | (0 << PC5);
-    Serial.println("Moving left");
-    // Set motor to half speed
-    // Right wheel spins forward at half speed
-    // Left wheel spins backward at half speed
-    //delayS(1);
-    //PORTC &= ~((1 << PC4) | (1 << PC5));
+    rightMotorForward();
+    leftMotorReverse();
+
+    Serial.println("Turning left");
+    
+    delayMs(1000);
 }
 
 void orientRight() {
-    Serial.println("Moving right");
-    // Set motor to half speed
-    // Right wheel spins backward at half speed
-    // Left sheel spins forward at half speed
-    //delayS(1);
-    //PORTC &= ~((1 << PC4) | (1 << PC5));
-}
-
-void turnAround() {
-    //PORTC |= (1 << PC4) | (0 << PC5);
-    // Set motor to half speed
-    // Right wheel spins backward at half speed
-    // Left sheel spins forward at half speed
-    //delayS(4);
-    //PORTC &= ~((1 << PC4) | (1 << PC5));
-    Serial.println("Turning around");
+    leftMotorForward();
+    rightMotorReverse();
+    Serial.println("Turning right");
+    
+    delayMs(1000);
 }
